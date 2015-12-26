704ba1700be794fa7a37c596584c80cf2a4041d4
Ekaterina Krivich <krivich.ekaterina@gmail.com>
2015-12-26 14:07:49 +0300

    count empidemic number

--- epidemic.go
+++ epidemic.go
@@ -26,22 +26,28 @@ func main() {
   hermits[1] = 1
 
   rand.Seed(time.Now().UTC().UnixNano())
-  whos_next := 1
+  current_sick := 1
+  whos_next := current_sick
+  whos_next = random_next(whos_next, current_sick)
 
-  for whos_next == 1 {
-		whos_next = rand.Intn(len(hermits)) + 1
+  for spread(whos_next) == 0 {
+    fmt.Println("one more")
+    current_sick = whos_next
+    whos_next = random_next(whos_next, current_sick) 
   }
-  fmt.Println(whos_next)
-	if spread(whos_next) == 0 {
-		// what should be done here?
-	}
+}
 
+func random_next(hermit_id int, current_sick int) int {
+  for hermit_id == current_sick {
+    hermit_id = rand.Intn(len(hermits)) + 1
+  }
+  return hermit_id
 }
 
 // returns 0 if next hermit is not sick yet
 func spread(hermit_id int) int {
 	// need to remember if he was sick
-	he_was_sick = hermits[hermit_id]
+	he_was_sick := hermits[hermit_id]
 	// now he is sick anyway
 	hermits[hermit_id] = 1
 	return he_was_sick

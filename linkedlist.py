7c2662ebd1ef63be1acddff99a250198a6b2b506
Ekaterina Krivich <ekaterinak@heathmont.net>
2017-07-04 08:59:27 +0300

    print list as a function

--- linkedlist.py
+++ linkedlist.py
@@ -3,13 +3,22 @@ class Node(object):
         self.data = data
         self.next = None
 
+class List(object):
+    def __init__(self, head):
+        self.head = head
+
+def printList(list):
+    ref = list.head
+    while(ref):
+        print(ref.data)
+        ref = ref.next
+
 head = Node(1)
+myList = List(head)
+
 node2 = Node(2)
 head.next = node2
 node3 = Node(3)
 node2.next = node3
 
-ref = head
-while(ref):
-    print(ref.data)
-    ref = ref.next
+printList(myList)

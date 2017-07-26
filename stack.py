99146c0e7f391340a496019a4ec07e818e4f07c8
Ekaterina Krivich <ekaterinak@heathmont.net>
2017-07-26 08:04:53 +0300

    new implementation

--- stack.py
+++ stack.py
@@ -4,12 +4,29 @@ class Node(object):
         self.next = None
 
 class Stack(object):
-    def __init__(self, head):
-        self.head = head
-        self.tail = head
+    def __init__(self):
+        self.head = None
+        self.tail = None
     def push(self, elem):
-        self.tail.next = elem
-        self.tail = elem
+        if (not self.head):
+            self.head = elem
+        else:
+            if (not self.tail):
+                self.head.next = elem
+                self.tail = elem
+            self.tail.next = elem
+            self.tail = elem
+    def pop(self):
+        current = self.head
+        if (not current.next):
+            self.head = None
+            return current
+        if (not current):
+            return None
+        while (current.next != self.tail):
+            current = current.next
+        self.tail = current
+        return self.tail
     def print(self):
         current = self.head
         while(current):
@@ -20,8 +37,14 @@ node1 = Node(1)
 node2 = Node(2)
 node3 = Node(3)
 
-stack = Stack(node1)
+stack = Stack()
+stack.push(node1)
 stack.push(node2)
 stack.push(node3)
+
+stack.print()
+
+stack.pop()
+stack.pop()
 
 stack.print()

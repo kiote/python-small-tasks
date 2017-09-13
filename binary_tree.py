b037f305e045a5b96dcacc3128c2f1675e40bae8
Ekaterina Krivich <ekaterinak@heathmont.net>
2017-09-13 14:39:41 +0300

    init tree find

--- binary_tree.py
+++ binary_tree.py
@@ -8,18 +8,20 @@ class Tree(object):
     def __init__(self, head):
         self.head = head
 
-node1 = Node(5)
-node2 = Node(3)
-node3 = Node(10)
-node4 = Node(1)
-node5 = Node(4)
-node6 = Node(7)
-node7 = Node(12)
+def initTree():
+    print("Inint tree:")
+    node1 = Node(5)
+    node2 = Node(3)
+    node3 = Node(10)
+    node4 = Node(1)
+    node5 = Node(4)
+    node6 = Node(7)
+    node7 = Node(12)
 
-tree = Tree(node1)
-node1.left = node2
-node1.right = node3
-node2.left = node4
-node2.right = node5
-node3.left = node6
-node3.right = node7
+    tree = Tree(node1)
+    node1.left = node2
+    node1.right = node3
+    node2.left = node4
+    node2.right = node5
+    node3.left = node6
+    node3.right = node7
